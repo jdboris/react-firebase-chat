3172416d6da4b976f0f6f65391456e91f565cddf
Joe <54695266+jdboris@users.noreply.github.com>
2021-06-15 12:40:59 -0400

    Added usernames and @'s
    -added usernames to messages
    -added the ability to @ someone manually or by clicking their message

--- src/components/chat-room.jsx
+++ src/components/chat-room.jsx
@@ -16,16 +16,19 @@ export function ChatRoom(props) {
 
   const [formValue, setFormValue] = useState("");
 
+  let messageInput = null;
+
   const sendMessage = async (e) => {
     e.preventDefault();
     const text = formValue;
     setFormValue("");
 
-    const { uid, photoURL } = auth.currentUser;
+    const { uid, photoURL, displayName } = auth.currentUser;
 
     await messagesRef.add({
       text: text,
       createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      username: displayName,
       uid,
       photoURL,
     });
@@ -38,13 +41,24 @@ export function ChatRoom(props) {
       <div className={styles["chat-room"]}>
         <span ref={dummy}></span>
         {messages &&
-          messages
-            .reverse()
-            .map((msg) => <ChatMessage key={msg.id} message={msg} />)}
+          messages.reverse().map((msg) => (
+            <ChatMessage
+              key={msg.id}
+              message={msg}
+              onClick={(targetUsername) => {
+                setFormValue(formValue + " @" + targetUsername + " ");
+                messageInput.focus();
+              }}
+            />
+          ))}
       </div>
 
       <form onSubmit={sendMessage}>
         <textarea
+          ref={(input) => {
+            messageInput = input;
+          }}
+          autoFocus
           value={formValue}
           onChange={(e) => setFormValue(e.target.value)}
           placeholder="Type here to send a message"
@@ -67,19 +81,59 @@ export function ChatRoom(props) {
 }
 
 function ChatMessage(props) {
-  const { text, uid, photoURL, createdAt } = props.message;
+  let { text, uid, photoURL, createdAt, username } = props.message;
   const messageClass = uid === auth.currentUser.uid ? "sent" : "received";
+  let mouseDownSpot = null;
+
+  console.log(text);
+  const doesMentionCurrentUser = text.includes(
+    `@${auth.currentUser.displayName} `
+  );
+
+  let parts = text.split(`@${auth.currentUser.displayName} `);
+  let message = [parts[0]];
+
+  for (let i = 1; i < parts.length; i++) {
+    message.push(<strong>@{auth.currentUser.displayName} </strong>);
+    message.push(parts[i]);
+  }
 
   return (
     <>
-      <div className={styles["message"] + " " + styles[messageClass]}>
+      <div
+        className={
+          styles["message"] +
+          " " +
+          styles[messageClass] +
+          " " +
+          (doesMentionCurrentUser ? styles["mention"] : "")
+        }
+        onMouseDown={(e) => {
+          mouseDownSpot = { x: e.pageX, y: e.pageY };
+        }}
+        onMouseUp={(e) => {
+          if (mouseDownSpot) {
+            let a = mouseDownSpot.x - e.pageX;
+            let b = mouseDownSpot.y - e.pageY;
+
+            let distance = Math.sqrt(a * a + b * b);
+
+            if (distance <= 2) {
+              props.onClick(username);
+              mouseDownSpot = null;
+            }
+          }
+        }}
+      >
         <img src={photoURL || "https://i.imgur.com/h2yCi23.jpg"} />
         <span className={styles["message-details"]}>
           <span className={styles["message-timestamp"]}>
             {createdAt && createdAt.toDate().toLocaleString()}
           </span>
         </span>
-        <p>{text}</p>
+        <p>
+          {username}: {message}
+        </p>
       </div>
     </>
   );
