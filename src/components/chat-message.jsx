78d89be809972407d5d45af81cd68e2c1e9f059b
Joe <54695266+jdboris@users.noreply.github.com>
2021-07-05 02:16:00 -0400

    refactoring
    -moved the ChatMessage component out of chat-room.jsx

---
+++ src/components/chat-message.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import styles from "../css/chat-room.module.css";
+
+import { auth } from "../app";
+
+export function ChatMessage(props) {
+  let { text, uid, photoURL, createdAt, username } = props.message;
+  let { claims } = props.idToken;
+  const messageClass = uid === auth.currentUser.uid ? "sent" : "received";
+
+  let mouseDownSpot = null;
+
+  const doesMentionCurrentUser = text.includes(
+    `@${auth.currentUser.displayName} `
+  );
+
+  let parts = text.split(`@${auth.currentUser.displayName} `);
+  let message = [parts[0]];
+
+  function banUser() {
+    props.bannedUsersRef.doc(props.message.uid).set({});
+  }
+
+  function deleteMessage() {
+    props.messagesRef.doc(props.message.id).update({ isDeleted: true });
+  }
+
+  for (let i = 1; i < parts.length; i++) {
+    message.push(<strong>@{auth.currentUser.displayName} </strong>);
+    message.push(parts[i]);
+  }
+
+  return (
+    <>
+      <div
+        className={
+          styles["message"] +
+          " " +
+          styles[messageClass] +
+          " " +
+          (doesMentionCurrentUser ? styles["mention"] : "")
+        }
+        onMouseDown={(e) => {
+          mouseDownSpot = { x: e.pageX, y: e.pageY };
+        }}
+        onMouseUp={(e) => {
+          // Left click
+          if (e.button == 0 && mouseDownSpot) {
+            let a = mouseDownSpot.x - e.pageX;
+            let b = mouseDownSpot.y - e.pageY;
+
+            let distance = Math.sqrt(a * a + b * b);
+
+            if (distance <= 2) {
+              props.onClick(username);
+              mouseDownSpot = null;
+            }
+          }
+        }}
+      >
+        <img src={photoURL || "https://i.imgur.com/h2yCi23.jpg"} />
+        <span className={styles["message-details"]}>
+          {claims.isModerator ? <button onClick={banUser}>X</button> : ""}
+          {claims.isModerator ? <button onClick={deleteMessage}>X</button> : ""}
+          <span className={styles["message-timestamp"]}>
+            {createdAt && createdAt.toDate().toLocaleString()}
+          </span>
+        </span>
+        <p>
+          <span className={styles["message-username"]}>{username}</span>:{" "}
+          {message}
+        </p>
+      </div>
+    </>
+  );
+}
--- src/components/chat-room.jsx
+++ src/components/chat-room.jsx
@@ -3,12 +3,11 @@ import styles from "../css/chat-room.module.css";
 import MenuIcon from "@material-ui/icons/Menu";
 
 import firebase from "firebase/app";
-
-import { useCollectionData } from "react-firebase-hooks/firestore";
-
 import { firestore, auth } from "../app";
 
+import { useCollectionData } from "react-firebase-hooks/firestore";
 import { presence } from "../presence";
+import { ChatMessage } from "./chat-message";
 
 let uid = null;
 
@@ -203,78 +202,6 @@ export function ChatRoom(props) {
       ) : (
         ""
       )}
-    </>
-  );
-}
-
-function ChatMessage(props) {
-  let { text, uid, photoURL, createdAt, username } = props.message;
-  let { claims } = props.idToken;
-  const messageClass = uid === auth.currentUser.uid ? "sent" : "received";
-
-  let mouseDownSpot = null;
-
-  const doesMentionCurrentUser = text.includes(
-    `@${auth.currentUser.displayName} `
-  );
-
-  let parts = text.split(`@${auth.currentUser.displayName} `);
-  let message = [parts[0]];
-
-  function banUser() {
-    props.bannedUsersRef.doc(props.message.uid).set({});
-  }
-
-  function deleteMessage() {
-    props.messagesRef.doc(props.message.id).update({ isDeleted: true });
-  }
-
-  for (let i = 1; i < parts.length; i++) {
-    message.push(<strong>@{auth.currentUser.displayName} </strong>);
-    message.push(parts[i]);
-  }
-
-  return (
-    <>
-      <div
-        className={
-          styles["message"] +
-          " " +
-          styles[messageClass] +
-          " " +
-          (doesMentionCurrentUser ? styles["mention"] : "")
-        }
-        onMouseDown={(e) => {
-          mouseDownSpot = { x: e.pageX, y: e.pageY };
-        }}
-        onMouseUp={(e) => {
-          // Left click
-          if (e.button == 0 && mouseDownSpot) {
-            let a = mouseDownSpot.x - e.pageX;
-            let b = mouseDownSpot.y - e.pageY;
-
-            let distance = Math.sqrt(a * a + b * b);
-
-            if (distance <= 2) {
-              props.onClick(username);
-              mouseDownSpot = null;
-            }
-          }
-        }}
-      >
-        <img src={photoURL || "https://i.imgur.com/h2yCi23.jpg"} />
-        <span className={styles["message-details"]}>
-          {claims.isModerator ? <button onClick={banUser}>X</button> : ""}
-          {claims.isModerator ? <button onClick={deleteMessage}>X</button> : ""}
-          <span className={styles["message-timestamp"]}>
-            {createdAt && createdAt.toDate().toLocaleString()}
-          </span>
-        </span>
-        <p>
-          <span className={styles["message-username"]}>{username}</span>:{" "}
-          {message}
-        </p>
-      </div>
     </>
   );
 }
