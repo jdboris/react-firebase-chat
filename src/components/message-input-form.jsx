0317755b7f9574200ddd7df0b2dbd2e6480fb4b1
Joe <54695266+jdboris@users.noreply.github.com>
2021-08-13 05:39:43 -0400

    vulnerabilities, embedding images, uploading images
    -ran npm audit fix to update packages for security
    -changed storage.rules to prevent uploading svgs for security (to prevent embedded javascript)
    -allowed embedding external images
    -allowed uploading images

--- src/components/message-input-form.jsx
+++ src/components/message-input-form.jsx
@@ -1,65 +1,85 @@
+import { default as React } from "react";
+import CameraIcon from "@material-ui/icons/CameraAlt";
 import TextFormatIcon from "@material-ui/icons/TextFormat";
-import React from "react";
 import { hexToRgb } from "../color";
 import styles from "../css/chat-room.module.css";
+import { uploadFile } from "../storage";
 
 export function MessageInputForm(props) {
   return (
-    <form
-      onSubmit={props.sendMessage}
-      style={
-        props.userStyles
-          ? {
-              backgroundColor: `rgba(${hexToRgb(props.msgBgColor)},${
-                props.msgBgTransparency
-              })`,
+    <form onSubmit={props.sendMessage}>
+      <label>
+        <CameraIcon
+          className={styles["camera-icon"] + " " + styles["pointer"]}
+        />
+        <input
+          type="file"
+          onChange={async (e) => {
+            const file = e.target.files[0];
+            const url = await uploadFile(file);
+            if (url) {
+              props.setMessageValue(props.messageValue + " " + url);
             }
-          : {}
-      }
-    >
+          }}
+        />
+      </label>
+
       <div
-        className={styles["message-background-image"]}
+        className={styles["input-wrapper"]}
         style={
           props.userStyles
             ? {
-                backgroundImage: `url(${props.msgBgImg})`,
-                backgroundRepeat: props.msgBgRepeat,
-                backgroundPosition: props.msgBgPosition,
-                opacity: props.msgBgImgTransparency,
+                backgroundColor: `rgba(${hexToRgb(props.msgBgColor)},${
+                  props.msgBgTransparency
+                })`,
               }
             : {}
         }
-      ></div>
-      <textarea
-        ref={(input) => {
-          props.messageInput(input);
-        }}
-        autoFocus
-        value={props.messageValue}
-        onChange={(e) => props.setMessageValue(e.target.value)}
-        placeholder="Type here to send a message"
-        onKeyPress={(e) => {
-          if (e.key === "Enter" && !e.shiftKey) {
-            e.target.form.dispatchEvent(
-              new Event("submit", {
-                cancelable: true,
-                // BUGFIX: https://github.com/final-form/react-final-form/issues/878#issuecomment-745364350
-                bubbles: true,
-              })
-            );
-            e.preventDefault();
+      >
+        <div
+          className={styles["message-background-image"]}
+          style={
+            props.userStyles
+              ? {
+                  backgroundImage: `url(${props.msgBgImg})`,
+                  backgroundRepeat: props.msgBgRepeat,
+                  backgroundPosition: props.msgBgPosition,
+                  opacity: props.msgBgImgTransparency,
+                }
+              : {}
           }
-        }}
-        style={
-          props.userStyles
-            ? {
-                fontFamily: props.font.style,
-                fontSize: props.fontSize,
-                color: props.fontColor,
-              }
-            : {}
-        }
-      ></textarea>
+        ></div>
+        <textarea
+          ref={(input) => {
+            props.messageInput(input);
+          }}
+          autoFocus
+          value={props.messageValue}
+          onChange={(e) => props.setMessageValue(e.target.value)}
+          placeholder="Type here to send a message"
+          onKeyPress={(e) => {
+            if (e.key === "Enter" && !e.shiftKey) {
+              e.target.form.dispatchEvent(
+                new Event("submit", {
+                  cancelable: true,
+                  // BUGFIX: https://github.com/final-form/react-final-form/issues/878#issuecomment-745364350
+                  bubbles: true,
+                })
+              );
+              e.preventDefault();
+            }
+          }}
+          style={
+            props.userStyles
+              ? {
+                  fontFamily: props.font.style,
+                  fontSize: props.fontSize,
+                  color: props.fontColor,
+                }
+              : {}
+          }
+        ></textarea>
+      </div>
 
       <TextFormatIcon
         className={
