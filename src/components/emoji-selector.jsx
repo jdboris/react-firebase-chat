a5983fbaf0dee2fb80a6acae0d1638b2b8814041
Joe <54695266+jdboris@users.noreply.github.com>
2021-08-14 14:10:51 -0400

    emoji search
    -added a searchbar in emoji selector

--- src/components/emoji-selector.jsx
+++ src/components/emoji-selector.jsx
@@ -1,11 +1,23 @@
 import emoji from "emoji-dictionary";
 import React from "react";
+import { useState } from "react";
 import styles from "../css/emoji-selector.module.css";
 
 export function EmojiSelector(props) {
+  const [emojis, setEmojis] = useState(emoji.unicode);
+
   return (
-    <div className={styles["emoji-selector"]} shouldcomponentupdate={false}>
-      {emoji.unicode.map((emojiChar) => (
+    <div className={styles["emoji-selector"]}>
+      <input
+        onInput={(e) => {
+          setEmojis(
+            emoji.unicode.filter((emojiChar, i) => {
+              return emoji.names[i].includes(e.target.value);
+            })
+          );
+        }}
+      />
+      {emojis.map((emojiChar) => (
         <span
           onClick={() => {
             props.onSelect(emojiChar);
