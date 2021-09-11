d1ac9d925903e231af97b9d89c4713a92e22d8b7
Joe <54695266+jdboris@users.noreply.github.com>
2021-09-11 11:49:00 -0400

    Premium upgrade with Firebase Stripe Subscriptions Extension
    -started adding Premium upgrade with Firebase Stripe Subscriptions Extension

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import "firebase/analytics";
 import firebase from "firebase/app";
+import { loadStripe } from "@stripe/stripe-js";
 import "firebase/auth";
 import "firebase/firestore";
 import "firebase/functions";
@@ -56,32 +57,81 @@ const messagesRef = firestore.collection("messages");
 
 export const banUser = firebase.functions().httpsCallable("banUser");
 export const unbanUser = firebase.functions().httpsCallable("unbanUser");
+export const getCustomerPortalLink = firebase
+  .functions()
+  .httpsCallable("ext-firestore-stripe-subscriptions-createPortalLink");
+
+export async function sendToStripe(uid, priceId) {
+  return firestore
+    .collection("users")
+    .doc(uid)
+    .collection("checkout_sessions")
+    .add({
+      price: priceId, // todo price Id from your products price in the Stripe Dashboard
+      success_url: window.location.origin, // return user to this screen on successful purchase
+      cancel_url: window.location.origin, // return user to this screen on failed purchase
+    })
+    .then((docRef) => {
+      // Wait for the checkoutSession to get attached by the extension
+      docRef.onSnapshot(async (snap) => {
+        const { error, sessionId } = snap.data();
+
+        if (error) {
+          // Show an error to your customer and inspect
+          // your Cloud Function logs in the Firebase console.
+          console.error(`An error occurred: ${error.message}`);
+        }
+
+        if (sessionId) {
+          // We have a session, let's redirect to Checkout
+          console.log(`redirecting`);
+          const stripe = await loadStripe(
+            process.env.REACT_APP_STRIPE_PUBLIC_API_KEY
+          );
+
+          await stripe.redirectToCheckout({ sessionId });
+        }
+      });
+    });
+}
 
 function App() {
   const [user] = useAuthState(auth);
   const email = user && !user.isAnonymous ? user.email : "";
-  const [dmMessagesRef, setDmMessagesRef] = useState( null );
+  const [dmMessagesRef, setDmMessagesRef] = useState(null);
 
   // NOTE: This is a safe usage of displayName
-  const header = (dmMessagesRef && user) ? dmMessagesRef.parent.id
-                .split(":")
-                .filter((e) => e !== user.displayName)
-                .toString() : "";
+  const header =
+    dmMessagesRef && user
+      ? dmMessagesRef.parent.id
+          .split(":")
+          .filter((e) => e !== user.displayName)
+          .toString()
+      : "";
 
   return (
     <div className={styles["chat-app"]}>
       {user ? (
         <>
-          {dmMessagesRef ? 
-            <ChatRoom user={user} messagesRef={dmMessagesRef} setDmMessagesRef={setDmMessagesRef} header={header} dms={true} /> 
-            :
-            <ChatRoom user={user} messagesRef={messagesRef} setDmMessagesRef={setDmMessagesRef} />
-          }
+          {dmMessagesRef ? (
+            <ChatRoom
+              user={user}
+              messagesRef={dmMessagesRef}
+              setDmMessagesRef={setDmMessagesRef}
+              header={header}
+              dms={true}
+            />
+          ) : (
+            <ChatRoom
+              user={user}
+              messagesRef={messagesRef}
+              setDmMessagesRef={setDmMessagesRef}
+            />
+          )}
         </>
       ) : (
         <SignInForm email={email} />
       )}
-
     </div>
   );
 }
