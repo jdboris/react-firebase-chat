47b7569078ed2cc8a7f7d49826d6ed4b3d2074ae
Joe <54695266+jdboris@users.noreply.github.com>
2021-09-15 03:52:41 -0400

    premium, premium emojis
    -made premium upgrade work
    -added restriction on premium emojis
    -added chat-logout option in query string to refresh token for custom claims (like premium)

--- src/components/emoji-selector.jsx
+++ src/components/emoji-selector.jsx
@@ -4,6 +4,45 @@ import styles from "../css/emoji-selector.module.css";
 
 export function EmojiSelector(props) {
   const [searchValue, setSearchValue] = useState("");
+  const freeNames = [
+    "slightly_smiling_face",
+    "neutral_face",
+    "slightly_frowning_face",
+    "smile",
+    "laughing",
+    "angry",
+    "rage",
+    "laughing",
+    "stuck_out_tongue_winking_eye",
+    "kssing_heart",
+    "open_mouth",
+    "cry",
+    "wink",
+    "sunglasses",
+    "upside_down_face",
+    "vomiting",
+    "sleeping",
+    "heart",
+    "broken_heart",
+    "blush",
+    "wave",
+    "v",
+    "shushing",
+    "unamused",
+    "roll_eyes",
+    "hamburger",
+    "stop_sign",
+    "star",
+
+    // "fire",
+    // "100",
+    // "peach",
+    // "eggplant",
+  ];
+
+  const freeCodes = emoji.unicode.filter((code, i) => {
+    return freeNames.includes(emoji.names[i]);
+  });
 
   return (
     <div className={styles["emoji-selector"]}>
@@ -16,22 +55,28 @@ export function EmojiSelector(props) {
         placeholder="smile, thinking, glasses, etc..."
       />
       <section>
-        {emoji.unicode.reduce((accumulator, emojiChar, i) => {
-          if (searchValue == "" || emoji.names[i].includes(searchValue)) {
-            accumulator.push(
-              <span
-                title={emoji.names[i]}
-                onClick={() => {
-                  props.onSelect(emojiChar);
-                }}
-              >
-                {emojiChar}
-              </span>
-            );
-          }
+        {(props.premium ? emoji.unicode : freeCodes).reduce(
+          (accumulator, emojiChar, i) => {
+            if (
+              searchValue == "" ||
+              (props.premium ? emoji.names : freeNames)[i].includes(searchValue)
+            ) {
+              accumulator.push(
+                <span
+                  title={(props.premium ? emoji.names : freeNames)[i]}
+                  onClick={() => {
+                    props.onSelect(emojiChar);
+                  }}
+                >
+                  {emojiChar}
+                </span>
+              );
+            }
 
-          return accumulator;
-        }, [])}
+            return accumulator;
+          },
+          []
+        )}
       </section>
     </div>
   );
