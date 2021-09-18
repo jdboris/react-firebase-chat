f94b22662e531257f66fec816604c9da5cc236e7
Joe <54695266+jdboris@users.noreply.github.com>
2021-09-18 12:17:37 -0400

    premium dialog
    -added PremiumDialog for subscribing to Premium and managing subscription

---
+++ src/components/premium-dialog.jsx
@@ -0,0 +1,114 @@
+import CloseIcon from "@material-ui/icons/Close";
+import { default as React, useState } from "react";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import { firestore } from "../app";
+import styles from "../css/chat-room.module.css";
+import { sendToCustomerPortal, sendToStripe } from "../stripe";
+
+export function PremiumDialog(props) {
+  const { premium, uid, stripeLink } = props;
+  const [period, setPeriod] = useState(3);
+  const query = firestore
+    .collection("users")
+    .doc(uid)
+    .collection("subscriptions")
+    .where("status", "==", "active")
+    .where("role", "==", "premium");
+
+  const [subscriptions, loading, error] = useCollectionData(query);
+  console.log(error);
+  console.log(subscriptions);
+
+  return (
+    props.open && (
+      <div className={styles["dialog"] + " " + styles["premium-dialog"]}>
+        <header>
+          Premium
+          <CloseIcon
+            onClick={() => {
+              props.requestClose();
+            }}
+          />
+        </header>
+        <main>
+          {premium ? (
+            <>
+              {subscriptions.map((subscription) => {
+                const expiration = new Date(Date.UTC(1970, 0, 1));
+                expiration.setUTCSeconds(
+                  subscription.current_period_end.seconds
+                );
+
+                const formatter = new Intl.DateTimeFormat(undefined, {
+                  timeStyle: "short",
+                  dateStyle: "short",
+                });
+
+                return (
+                  <>Your subscription expires {formatter.format(expiration)}</>
+                );
+              })}
+
+              <div>
+                <button
+                  onClick={(e) => {
+                    e.preventDefault();
+                    sendToCustomerPortal();
+                  }}
+                >
+                  Manage Subscription
+                </button>
+              </div>
+            </>
+          ) : (
+            <>
+              Upgrade to a Premium account for perks like more emojis, more
+              message style options, and more...
+              <form
+                onSubmit={(e) => {
+                  console.log(process.env.REACT_APP_STRIPE_3_MONTH_PRICE_ID);
+                  e.preventDefault();
+                  sendToStripe(uid, e.target.period.value);
+                }}
+              >
+                <label>
+                  <input
+                    type="radio"
+                    name="period"
+                    value={process.env.REACT_APP_STRIPE_3_MONTH_PRICE_ID}
+                    onChange={() => setPeriod(3)}
+                    defaultChecked
+                  />{" "}
+                  3 Months - $2/month
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    name="period"
+                    value={process.env.REACT_APP_STRIPE_6_MONTH_PRICE_ID}
+                    onChange={() => setPeriod(6)}
+                  />{" "}
+                  6 Months - $1.5/month
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    name="period"
+                    value={process.env.REACT_APP_STRIPE_12_MONTH_PRICE_ID}
+                    onChange={() => setPeriod(12)}
+                  />{" "}
+                  12 Months - $1/month
+                </label>
+                Billed every {period} months. Cancel any time.
+                <label>
+                  <button>Subscribe</button>
+                </label>
+              </form>
+            </>
+          )}
+        </main>
+        <footer></footer>
+      </div>
+    )
+  );
+}
---
+++ src/stripe.js
@@ -0,0 +1,51 @@
+import { loadStripe } from "@stripe/stripe-js";
+import { firestore } from "./app";
+import firebase from "firebase/app";
+
+export async function sendToStripe(uid, priceId) {
+  const returnUrl = new URL(window.location);
+  // Force a logout to refresh the token for premium custom claims
+  returnUrl.searchParams.set("chat-logout", "1");
+
+  return firestore
+    .collection("users")
+    .doc(uid)
+    .collection("checkout_sessions")
+    .add({
+      price: priceId, // price Id from your products price in the Stripe Dashboard
+      success_url: returnUrl.href, // return user to this screen on successful purchase
+      cancel_url: window.location.href, // return user to this screen on failed purchase
+    })
+    .then((docRef) => {
+      // Wait for the checkoutSession to get attached by the extension
+      docRef.onSnapshot(async (snap) => {
+        const { error, sessionId } = snap.data();
+
+        if (error) {
+          // Show an error to your customer and inspect
+          // your Cloud Function logs in the Firebase console.
+          console.error(`An error occurred: ${error.message}`);
+        }
+
+        if (sessionId) {
+          // We have a session, let's redirect to Checkout
+          console.log(`redirecting`);
+          const stripe = await loadStripe(
+            process.env.REACT_APP_STRIPE_PUBLIC_API_KEY
+          );
+
+          await stripe.redirectToCheckout({ sessionId });
+        }
+      });
+    });
+}
+
+export async function sendToCustomerPortal() {
+  // had to update firebase.app().functions() to firebase.default.functions() and
+  // removed the region from the functions call (from stripe firebase extension docs)
+  const functionRef = firebase
+    .functions()
+    .httpsCallable("ext-firestore-stripe-subscriptions-createPortalLink");
+  const { data } = await functionRef({ returnUrl: window.location.origin });
+  window.location.assign(data.url);
+}
