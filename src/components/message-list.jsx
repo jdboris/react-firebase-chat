12277eb9af663433db63c58a949075da69b10385
Joe <54695266+jdboris@users.noreply.github.com>
2021-10-04 16:48:08 -0400

    MessageList pagination, styles
    -added pagination to MessageList
    -tweaked styles

--- src/components/message-list.jsx
+++ src/components/message-list.jsx
@@ -1,28 +1,106 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "../css/chat-room.module.css";
 import { ChatMessage } from "./chat-message";
 
 export function MessageList(props) {
+  const {
+    messagesRef,
+    defaultMessages,
+    stylesEnabled,
+    onMessageClick,
+    currentUser,
+    sentMsgCount,
+  } = props;
   const messageList = useRef();
+
+  const [messages, setMessages] = useState([]);
+  const [paused, setPaused] = useState(false);
+
+  useEffect(() => {
+    if (!paused) {
+      setMessages(defaultMessages || []);
+    }
+  }, [defaultMessages, paused]);
 
   useEffect(() => {
     messageList.current.scrollTop = 0;
-  }, [props.sentMsgCount]);
+  }, [sentMsgCount]);
 
   return (
     <section ref={messageList} className={styles["messages-section"]}>
-      {props.messages &&
+      <div className={styles["pagination-controls"]}>
+        {messages.length && messages[0].id !== defaultMessages[0].id && (
+          <button
+            onClick={() => {
+              setPaused(false);
+            }}
+          >
+            Jump to present
+          </button>
+        )}
+      </div>
+
+      {messages &&
         // Must make a copy because props are immutable
-        props.messages.map((msg) => (
+        messages.map((msg) => (
           <ChatMessage
             key={msg.id}
             message={msg}
-            stylesEnabled={props.stylesEnabled}
-            onClick={props.onMessageClick}
-            currentUser={props.currentUser}
-            messagesRef={props.messagesRef}
+            stylesEnabled={stylesEnabled}
+            onClick={onMessageClick}
+            currentUser={currentUser}
+            messagesRef={messagesRef}
           />
         ))}
+
+      <div className={styles["pagination-controls"]}>
+        {messages.length >= 25 && (
+          <button
+            onClick={async () => {
+              setPaused(true);
+              const query = messagesRef
+                .where("isDeleted", "==", false)
+                .orderBy("createdAt", "desc")
+                .startAfter(messages[messages.length - 1].createdAt)
+                .limit(25);
+
+              const snapshot = await query.get();
+              const newMessages = snapshot.docs.map((doc) => {
+                return { id: doc.id, ...doc.data() };
+              });
+              setMessages(newMessages);
+            }}
+          >
+            Older
+          </button>
+        )}
+
+        {messages.length && messages[0].id !== defaultMessages[0].id && (
+          <button
+            onClick={async () => {
+              setPaused(true);
+              const query = messagesRef
+                .where("isDeleted", "==", false)
+                .orderBy("createdAt", "desc")
+                .endBefore(messages[0].createdAt)
+                .limitToLast(25);
+
+              const snapshot = await query.get();
+              const newMessages = snapshot.docs.map((doc) => {
+                return { id: doc.id, ...doc.data() };
+              });
+
+              if (newMessages[0].id === defaultMessages[0].id) {
+                setPaused(false);
+              } else if (newMessages.length) {
+                setMessages(newMessages);
+              }
+            }}
+          >
+            Newer
+          </button>
+        )}
+      </div>
     </section>
   );
 }
