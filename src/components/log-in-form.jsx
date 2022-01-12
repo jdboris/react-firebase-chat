173e334acf776e3b58eb522cc610b9eb616cc847
Joe <54695266+jdboris@users.noreply.github.com>
2022-01-12 11:36:20 -0500

    Clear password after login

--- src/components/log-in-form.jsx
+++ src/components/log-in-form.jsx
@@ -113,6 +113,7 @@ export function LogInForm(props) {
             })
             .finally(() => {
               setLoading(false);
+              setPassword("");
             });
 
           props.requestClose();
