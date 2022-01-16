340b4135a678fc2ca1df929d493128ae36e98cf0
Joe <54695266+jdboris@users.noreply.github.com>
2022-01-16 01:58:34 -0500

    Improve input validation messages for login

--- src/utils/errors.js
+++ src/utils/errors.js
@@ -5,6 +5,8 @@ export function translateError(error) {
     error.message = "Invalid email/password.";
   } else if (error.code === "auth/user-not-found") {
     error.message = "Invalid email/password.";
+  } else if (error.code === "auth/missing-email") {
+    error.message = "Please enter your email address.";
   }
 
   if (!error.message) {
