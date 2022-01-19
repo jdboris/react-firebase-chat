063243d960bd7f13cab9b5e8b3e1121f0a4f2acc
Joe <54695266+jdboris@users.noreply.github.com>
2022-01-19 09:53:55 -0500

    Allow uploading images with drag & drop

---
+++ src/components/file-upload-overlay.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import styles from "../css/chat-room.module.css";
+import { translateError } from "../utils/errors";
+import { uploadFile } from "../utils/storage";
+import { timeout } from "../utils/utils";
+
+export function FileUploadOverlay(props) {
+  const [loading, setLoading] = useState(false);
+
+  return (
+    (props.isDraggedOn || loading) && (
+      <label
+        className={
+          styles["file-drag-overlay"] + " " + (loading ? styles["loading"] : "")
+        }
+        onDragOver={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+        }}
+        onDrop={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+
+          if (loading) return;
+          setLoading(true);
+          timeout(5000, async () => {
+            const files = e.dataTransfer.files;
+            if (!files.length) {
+              return;
+            }
+
+            const file = files[0];
+            const url = await uploadFile(file);
+
+            if (!url) {
+              throw new Error("Error uploading file.");
+            }
+            console.log(props.messageInput);
+            props.messageInput.focus();
+            props.setMessageValue(props.messageValue + " " + url + " ");
+          })
+            .catch((error) => {
+              props.setErrors([translateError(error).message]);
+            })
+            .finally(() => {
+              setLoading(false);
+              e.target.value = "";
+            });
+
+          props.setIsDraggedOn(false);
+        }}
+        onDragLeave={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          props.setIsDraggedOn(false);
+        }}
+        onDragEnd={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          props.setIsDraggedOn(false);
+        }}
+      >
+        <div>
+          <input type="file" />
+        </div>
+      </label>
+    )
+  );
+}
