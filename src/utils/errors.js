95ab0dda4af4bdcb3397067368ec28af1286282e
Joe <54695266+jdboris@users.noreply.github.com>
2022-01-19 23:49:24 -0500

    Add custom error message for auth/too-many-requests

--- src/utils/errors.js
+++ src/utils/errors.js
@@ -7,6 +7,8 @@ export function translateError(error) {
     error.message = "Invalid email/password.";
   } else if (error.code === "auth/missing-email") {
     error.message = "Please enter your email address.";
+  } else if (error.code === "auth/too-many-requests") {
+    error.message = "Too many failed attempts. Try again later.";
   }
 
   if (!error.message) {
